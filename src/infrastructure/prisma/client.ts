e38864bdc5d39fc783a647c95cebbfaa0e566aec
Gustavo Sousa <gustavodeltta@gmail.com>
2026-06-04 01:49:37 -0300

    fix: reverte para PrismaPg com config object e adiciona pool options

--- src/infrastructure/prisma/client.ts
+++ src/infrastructure/prisma/client.ts
@@ -1,17 +1,14 @@
 import { env } from "../../shared/env";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma";
-import { Pool } from "pg";
 
 function createPrismaClient() {
-  const pool = new Pool({
+  const adapter = new PrismaPg({
     connectionString: env.DATABASE_URL,
     max: 5,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 5000,
   });
-
-  const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 }
 
